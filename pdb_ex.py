7c706743e02056fc09fbe8f0a44bb0aac79ae4bc
WOON SI <superman.woonsi@gmail.com>
2021-06-12 13:02:50 +0630

    flask-hello-world

---
+++ pdb_ex.py
@@ -0,0 +1,23 @@
+import sys
+import pdb
+from random import choice, random
+
+random1 = [1,2,3,4,5,6,7,8,9,10,11,12]
+random2 = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+while True:
+    print("To exit this game type 'exit'")
+    num1 = choice(random2)
+    num2 = choice(random1)
+
+    answer = int(input("What is {} times {}?".format(num1, num2)))
+
+
+    if answer == "exit":
+        print("Now exiting game!")
+        sys.exit()
+
+    elif answer == num1 * num2:
+        print("Correct!")
+    else:
+        print("Wrong!")
